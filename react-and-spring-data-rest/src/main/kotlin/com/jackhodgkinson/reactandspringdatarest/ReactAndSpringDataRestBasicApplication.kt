705af98a0670ce99b946af2ff75dfeab0f166a84
Jack Hodgkinson <jdahodgkinson@gmail.com>
2020-10-12 13:01:22 +0100

    inited spring react project

---
+++ react-and-spring-data-rest/src/main/kotlin/com/jackhodgkinson/reactandspringdatarest/ReactAndSpringDataRestBasicApplication.kt
@@ -0,0 +1,11 @@
+package com.jackhodgkinson.reactandspringdatarest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactAndSpringDataRestBasicApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReactAndSpringDataRestBasicApplication>(*args)
+}
---
+++ react-and-spring-data-rest/src/test/kotlin/com/jackhodgkinson/reactandspringdatarest/ReactAndSpringDataRestBasicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jackhodgkinson.reactandspringdatarest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactAndSpringDataRestBasicApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
